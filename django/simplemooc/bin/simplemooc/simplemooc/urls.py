bab8346cf7e5ed1036d5ee6ab3926699d2ee7453
Elias Neves <elias-neves93@hotmail.com>
2018-01-25 22:19:19 -0200

    Adicionando projetos testes em Django.

--- django/simplemooc/bin/simplemooc/simplemooc/urls.py
+++ django/simplemooc/bin/simplemooc/simplemooc/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
+    path(r'^$', 'simplemooc.core.views.home', name='home'),
     path('admin/', admin.site.urls),
 ]
---
+++ testeDjango/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/elias/python_devops/testeDjango/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
