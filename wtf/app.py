51be5980d886e226c386c8d022ce7639fdb401d3
Elias Neves <elias-neves93@hotmail.com>
2018-02-13 10:37:21 -0200

    Sessions and Cookies

--- wtf/app.py
+++ wtf/app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask import render_template
 import json
-from flask import make_response
+from flask import make_response, abort
 from flask import jsonify
 from flask import request, current_app
-
+import os
+from werkzeug import secure_filename
+from flask import request, redirect, url_for
+from flask import Flask, session, redirect, url_for, escape, request
 
 app = Flask('wtf')
 app = Flask(__name__.split('.')[0])
 
-@app.route("/")
 
+@app.route("/")
 def hello_world():
     return "Hello World! <strong>I am learning Flask</strong>", 200
 
@@ -59,5 +62,31 @@ def show_config():
         args=querystring_args,
         vars=post_args
     )
+
+@app.route("/change_photo")
+def perfil():
+    item_id = request.form.get('item_id')
+    photo = request.files.get('photo')
+    if item_id and photo:
+        filename = secure_filename(photo.filename)
+        file_path =os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        photo.save(file_path)
+        banco_de_dados.get(item_id=item_id).update(photo_path=file_path)
+    return "Imagem atualizada", 201
+
+@app.route('/login')
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if valid_login(username, password):
+        response = make_response("Hello! Welcome back!")
+        response.set_cookie('username', username)
+        response.set_cookie('user_hash', get_hash(username))
+        ...
+        return response
+    else:
+        abort(403)
+
+
 
 app.run(debug=True, use_reloader=True)
